f9a9123bb1e33e857bb245805047d79f69db452d
Zhiyu Guo <zg915@nyu.edu>
2025-07-07 10:31:36 -0400

    change CORS setting

--- src/api/server.py
+++ src/api/server.py
@@ -14,13 +14,35 @@ app = FastAPI(
     version="1.0.0"
 )
 
-# Add CORS middleware
+
+# Configure CORS with extended settings
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5175",
+    "http://73.226.206.254:5175",
+    "https://project.mangrovesai.com",
+    "https://www.mangrovesai.com",
+    "https://mangrovesai.com"
+]
+
+# Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins, 
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=[
+        "Content-Type",
+        "Authorization",
+        "Accept",
+        "Origin",
+        "X-Requested-With",
+        "If-None-Match",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers"
+    ],
+    expose_headers=["*"],
+    max_age=600,  # Cache preflight requests for 10 minutes
 )
 
 # Include routers
